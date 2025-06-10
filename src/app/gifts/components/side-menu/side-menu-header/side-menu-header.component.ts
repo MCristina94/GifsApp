35b3cf619974ed10ce6b50bb4bd6ad90fd7beea6
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-10 12:53:39 -0500

    created gif list and item components

--- src/app/gifts/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifts/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '@enviroments/environment';
+// import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'gift-side-menu-header',
   imports: [],
   templateUrl: './side-menu-header.component.html',
 })
-export class SideMenuHeaderComponent {}
+export class SideMenuHeaderComponent {
+  envs = environment;
+}
