1a0a26cffad283df235dd1f983a0493c41ac4bae
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-10 11:14:34 -0500

    added side menu header and options components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifts/pages/dashboard-page/dashboard-page.component'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifts/pages/tendring-page/tendring-page.component'),
+      },
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifts/pages/search-page/search-page.component'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
---
+++ src/app/gifts/pages/tendring-page/tendring-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tendring-page',
+  imports: [],
+  templateUrl: './tendring-page.component.html',
+})
+export default class TendringPageComponent {}
